7943fa380bce07467b712e92b320ff0a1f6ecfa2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-03 10:52:12 +0000

    Deploy roataway/web-ui to github.com/roataway/web-ui.git:gh-pages

---
+++ precache-manifest.130dca122b7b4b06fb2fc823b8ef5dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f934a2a129b594ad51b6b1d57b35beed",
+    "url": "/web-ui/index.html"
+  },
+  {
+    "revision": "74913010de59fed50bba",
+    "url": "/web-ui/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "a467f700d4b2375e513d",
+    "url": "/web-ui/static/css/main.b494b0cd.chunk.css"
+  },
+  {
+    "revision": "74913010de59fed50bba",
+    "url": "/web-ui/static/js/2.605151ae.chunk.js"
+  },
+  {
+    "revision": "a467f700d4b2375e513d",
+    "url": "/web-ui/static/js/main.0fa0cbf9.chunk.js"
+  },
+  {
+    "revision": "208e8e02655fea127f76",
+    "url": "/web-ui/static/js/runtime~main.a5b64d15.js"
+  }
+]);
